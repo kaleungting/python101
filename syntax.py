c243c9f67a6b3ba392ddc97df0ee1b7218e9eea7
Ken Ting <kaleungting@gmail.com>
2020-12-20 00:05:36 -0500

    notes for 12/19

---
+++ syntax.py
@@ -0,0 +1,41 @@
+
+print
+# comment line
+""" commenting block of code """
+== == == == == == == == == == == == == == == == == == == == == ==
+Types of Number
+== == == == == == == == == == == == == == == == == == == == == ==
+int() - whole integers
+float() - decimal numbers
+complex() - numbers consisting of a real part and imaginary part
+
+Type-cast: converting from one type of number to another
+
+
+
+
+
+== == == == == == == == == == == == == == == == == == == == == ==
+Arithmetic
+== == == == == == == == == == == == == == == == == == == == == ==
+no spaces between number and operators
+    ex. print(1+num)
+
+
+Integers can be super large, but when multipled by float() it will be shown as scientific notation
+
+// - gives the other part of modulo (good for dividing and rounding down, no need to float it down)
+% - gives the remainder 
+
+print(47//8) ==> 5
+print(47%8) ==> 7
+
+== == == == == == == == == == == == == == == == == == == == == ==
+String
+== == == == == == == == == == == == == == == == == == == == == ==
+
+print(len("Spaghetti")) ==> 9
+
+Indexing into a string works -> print(("Spaghetti"[0])) ==> "S"
+Negative Indexing is allowed 
+
