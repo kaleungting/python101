73a71a397991ba7e48519fe995d04d4df7598299
Ken Ting <kaleungting@gmail.com>
2021-02-10 14:43:09 -0500

    section11

---
+++ python_mastery/section11/main.py
@@ -0,0 +1,16 @@
+from scripts.util import addNums, subtractNums
+# from scripts.util import *
+
+
+print(addNums(1, 2))
+print(subtractNums(2, 1))
+
+
+"""
+OR
+"""
+# from scripts.util import *
+from scripts import util
+
+print(util.addNums(1, 2))
+print(util.subtractNums(2, 1))
---
+++ python_mastery/section11/scripts/util.py
@@ -0,0 +1,6 @@
+def addNums(num1, num2):
+    return num1+num2
+
+
+def subtractNums(num1, num2):
+    return num1-num2
